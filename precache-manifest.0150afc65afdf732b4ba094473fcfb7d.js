03822afc7221c5a8c685e55885410ebd72d51671
Vitalik <vitalik791@gmail.com>
2020-02-05 23:24:09 +0200

    Updates

---
+++ precache-manifest.0150afc65afdf732b4ba094473fcfb7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15784d4a8203b00e0485fe9c2938d2b6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c57d96503ef46a5bb409",
+    "url": "/goit-react-hw-01-components/static/css/main.c06fd871.chunk.css"
+  },
+  {
+    "revision": "ff2e72ae14265a5b3d46",
+    "url": "/goit-react-hw-01-components/static/js/2.d3d32399.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-01-components/static/js/2.d3d32399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57d96503ef46a5bb409",
+    "url": "/goit-react-hw-01-components/static/js/main.f4e7aa60.chunk.js"
+  },
+  {
+    "revision": "aefc1cfc55af87542fcc",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.61c42416.js"
+  }
+]);
